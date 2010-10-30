ad94a97a2d7250251940a1b2fc505c11e21f501a
Guilherme Carvalho <guilherme@guava.com.br>
2010-10-30 11:17:05 -0300

    Bugfix.

--- utils/load_train_pedestrians.m
+++ utils/load_train_pedestrians.m
@@ -1,4 +1,4 @@
 if not(exist('pedgray', 'var') && exist('pededge', 'var'))
 	disp('Loading train pedestrians...');
-	[pedgray,pededge] = load_images_and_edges(dir_filenames(sprintf('data/%s/train/pedestrians'), '\.jpg', true));
+	[pedgray,pededge] = load_images_and_edges(dir_filenames(sprintf('data/%s/train/pedestrians', current_set), '\.jpg', true));
 end
